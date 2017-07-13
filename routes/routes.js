abbdd87f526de85075aba08b8b2d305a9d15802e
Davis Uhlig <duhlig@daviss-mbp.lan>
2017-07-13 09:04:48 -0400

    set boilerplate

--- routes/routes.js
+++ routes/routes.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema;
 const config = require("../config")[process.env.NODE_ENV || 'test']
 const nodeEnv = process.env.NODE_ENV || "development";
 const app = require('../app');
-const items = require('../models/items.js');
-const purchases = require('../models/purchases.js');
+const items = require('../models/cards.js');
+const purchases = require('../models/decks.js');
 const bodyParser = require("body-parser");
 
 
